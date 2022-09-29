bfa0cad968626393b1f5e42672bc7f0cd6562df8
Yinson Tso <yinson.tso@gmail.com>
2022-09-28 20:43:20 -0400

    End of Day3 in Class

---
+++ W1/W1D3/Prime Number.js
@@ -0,0 +1,15 @@
+function isPrime(num){
+    if (num < 2){
+        return false;  //if number is less than 2, it is not a prime so skip
+    }
+    for (let n = 2; n < num; n++){ //change the quotient by 1 everytime
+        console.log(n);
+        if (num % n === 0){ //if the number has a remainder of 0
+            return false;  //then it is not a prime
+        }
+    }
+    console.log("it is a prime number")
+    return true; //if passes all rules, then number is prime/true
+
+}
+isPrime(53)

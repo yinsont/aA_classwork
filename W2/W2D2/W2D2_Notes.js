bcc4727d12b3d1a055491f6ec367ff7ac02e0643
Yinson Tso <yinson.tso@gmail.com>
2022-10-04 20:37:50 -0400

    end of day 7

--- W2/W2D2/W2D2_Notes.js
+++ W2/W2D2/W2D2_Notes.js
@@ -1,9 +1,83 @@
 function catBuilder(name, color, toys) {
   return {
-    name: name,
-    color: color,
-    toys: toys,
+    //Output: { name: 'whiskers', color: 'black', toys: 'yarn' }
+    name,
+    color,
+    toys,
   };
 }
 
-catBuilder("whiskers", "black", "yarn");
+console.log(catBuilder("whiskers", "black", "yarn"));
+//---------------------------------------------------------------------------------------------------
+
+// Fuction Decomposition / Objects
+
+// function isPrime(max) {
+//   if (max < 2) {
+//     return false;
+//   }
+//   for (i = 1; i >= max; i++) {
+//     if (i % 2 === 0) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
+// function laligetSum(num) {
+//   let sum = 0;
+//   for (let i = 2; i <= num; i++) {
+//     if (isPrime(i)) {
+//       sum += i;
+//     }
+//   }
+//   return sum;
+// }
+// console.log(laligetSum(10));
+// console.log(laligetSum(17));
+
+//-------------------------------------------------------------------------------------------------------------------------------------
+
+// JS Datastructure
+// Mutable
+// Key - Vakue Pairs
+// key - Always a String
+// Value - Can be Anything
+// Keys are Unique
+// Objects are Unordered, arrays are ordered
+
+let cat = {
+  name: "whiskers",
+  color: "black",
+  toys: {
+    outside: ["ball", "whistle"],
+    inside: ["blanket"],
+  },
+};
+
+// cat["name"] = "Jael";
+// cat["age"] = 21;
+// console.log(cat);
+// console.log(cat["color"]);
+
+// cat.name;
+// cat.age;
+// cat.toys.outside[0];
+// console.log(cat.test);
+// let test = "name";
+// console.log(cat[test]);
+// console.log(cat.test);
+
+// console.log(cat.name);
+
+//------------------------------------------------------------------------------------------------
+//let through object
+
+function printObject(obj) {
+  for (let key in obj) {
+    // looping key is in obj
+    console.log(key); //looking for key
+    console.log(obj[key]); // looking for key's value
+  }
+}
+printObject(cat);
---
+++ W2/W2D2/tempCodeRunnerFile.js
@@ -0,0 +1,35 @@
+let cat = {
+//   name: "whiskers",
+//   color: "black",
+//   toys: {
+//     outside: ["ball", "whistle"],
+//     inside: ["blanket"],
+//   },
+// };
+
+// // cat["name"] = "Jael";
+// // cat["age"] = 21;
+// // console.log(cat);
+// // console.log(cat["color"]);
+
+// // cat.name;
+// // cat.age;
+// // cat.toys.outside[0];
+// // console.log(cat.test);
+// // let test = "name";
+// // console.log(cat[test]);
+// // console.log(cat.test);
+
+// // console.log(cat.name);
+
+// //------------------------------------------------------------------------------------------------
+// //let through object
+
+// function printObject(obj) {
+//   for (let key in obj) {
+//     // looping key is in obj
+//     console.log(key); //looking for key
+//     console.log(obj[key]); // looking for key's value
+//   }
+// }
+// printObject(cat);

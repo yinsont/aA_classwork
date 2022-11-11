f09daf179d5288c57cf401c8daf43c861abf6e80
Yinson Tso <yinson.tso@gmail.com>
2022-11-10 21:35:05 -0500

    allElseEqual2

---
+++ At Home Coding Practice/allElseEqual.js
@@ -0,0 +1,17 @@
+function allElseEqual(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++){
+        sum += arr[i]
+    }
+    if (arr.includes(sum/2)) {
+        for (let n = 0; n < arr.length; n++){
+            if (arr[n] === sum/2) return arr[n]
+        }
+    } else {
+        return null
+    }
+}
+
+console.log(allElseEqual([2, 4, 3, 10, 1])); // 10
+console.log(allElseEqual([6, 3, 5, -9, 1])); // 3
+console.log(allElseEqual([1, 2, 3, 4]));     // null

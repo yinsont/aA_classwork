4315a94a9584c4a5b354874b067ed56fcb9cad8c
Yinson Tso <yinson.tso@gmail.com>
2022-12-05 02:48:36 -0500

    notes

---
+++ At Home Coding Practice/Objects/lectue.js
@@ -0,0 +1,11 @@
+let testObject = {};
+
+console.log(testObject) //empty
+
+testObject['num'] = 10
+
+console.log(testObject) //puts in key 'num' and gave it value 10
+
+testObject.num2 = 20 //easier way to add key/value
+
+console.log(testObject) 

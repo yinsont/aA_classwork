a62a74e59196e875047fd9d1b227185e8bc8faa6
Yinson Tso <yinson.tso@gmail.com>
2022-12-08 17:49:48 -0500

    obj practice 2

--- At Home Coding Practice/Obj Practice/1.js
+++ At Home Coding Practice/Obj Practice/1.js
@@ -1,17 +1,22 @@
-// Write a JavaScript program to list the properties of a JavaScript object. Go to the editor
+// Write a JavaScript program to delete the rollno property from the following object.
+// Also print the object before or after deleting the property.
 // Sample object:
-// var student = {
-// name : "David Rayy",
-// sclass : "VI",
-// rollno : 12 };
-// Sample Output: name,sclass,rollno
-
-
-var student = {
-name : "David Rayy",
-sclass : "VI",
-rollno : 12 };
-
-for (let key in student){
-    console.log(key)
+let student = {
+  name: "David Rayy",
+  sclass: "VI",
+  rollno: 12,
+};
+//-----------------------------------------
+for (let key in student) {
+  console.log(key);
 }
+//* Output: name, sclass, rollno
+//-----------------------------------
+console.log(student["sclass"]);
+
+//* Output: VI
+//-----------------------------
+console.log(Object.keys(student).length); 
+
+//* Output: length of object: 3
+//---------------------------------------

939732c37918db1213c113d3a376a620157c60c7
Yinson Tso <yinson.tso@gmail.com>
2023-01-04 21:50:56 -0500

    sameCharCollapse.js

--- At Home Coding Practice/Strings/sameCharCollapse.js
+++ At Home Coding Practice/Strings/sameCharCollapse.js
@@ -1,10 +1,15 @@
 function sameCharCollapse(str) {
-  for (let i = 0; i < str.length; i++){
-    
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i + 1]) {
+      str = str.slice(0, i) + str.slice(i + 2, str.length);
+      i = 0;
+    }
   }
+  return str;
 }
 
-console.log(sameCharCollapse("zzzxaaxy"));  // "zy"
+console.log(sameCharCollapse("zzzxaaxy")); // "zy"
 // because zzzxaaxy -> zxaaxy -> zxxy -> zy
 console.log(sameCharCollapse("uqrssrqvtt")); // "uv"
 // because uqrssrqvtt -> uqrrqvtt -> uqqvtt -> uvtt -> uv
+console.log(7%25)

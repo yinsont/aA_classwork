11cc721968b2480c9626cc2c568bc2c21c47c0e9
Yinson Tso <yinson.tso@gmail.com>
2023-01-16 20:14:37 -0500

    dogYears

---
+++ At Home Coding Practice/Home Projects/dogYears.js
@@ -0,0 +1,13 @@
+function dogYears (n){
+    if (n > 2){
+        ans = (10.5 * 2) + (4 * (n-2))
+        return ans
+    } else if ((n > 0) && (n < 2))  {
+        ans = n * 10.5
+        return ans
+    } else {
+        return null
+    }
+}
+
+console.log(dogYears())

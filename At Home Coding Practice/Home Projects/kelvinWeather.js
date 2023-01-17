0cedc0d0a05aab2fa475f5c570545e315b158f8f
Yinson Tso <yinson.tso@gmail.com>
2023-01-16 20:03:55 -0500

    kelvinWeather

---
+++ At Home Coding Practice/Home Projects/kelvinWeather.js
@@ -0,0 +1,11 @@
+let kelvin = 0
+
+let celsius = kelvin - 273
+
+let fahrenheit = Math.floor(celsius * (9/5) + 32)
+
+let newton = celsius * (33/100)
+
+console.log(`The temperature is ${fahrenheit} degrees Farhenheit"`)
+
+console.log(newton)

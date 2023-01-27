bdef3a93c6c3b4188d0f78c38e09046e6f093631
Yinson Tso <yinson.tso@gmail.com>
2023-01-27 17:16:12 -0500

    raceDay

--- At Home Coding Practice/Home Projects/Code Academy/raceDay.js
+++ At Home Coding Practice/Home Projects/Code Academy/raceDay.js
@@ -1,5 +1,25 @@
+// let raceNumber = Math.floor(Math.random() * 1000);
+
+// let food = true
+
+// raceNumber > 500 ? console.log(true) : console.log(false)
+
 let raceNumber = Math.floor(Math.random() * 1000);
 
-let food = false
+let registeredEarly = true;
 
-food ? console.log(true) : console.log(false)
+let runnerAge = 17;
+
+runnerAge > 18 ? (raceNumber += 1000) : null;
+
+if (runnerAge > 18 && registeredEarly === true) {
+  console.log(`Number ${raceNumber}, your race begins at 9:30 AM.`);
+} else if (runnerAge > 18 && registeredEarly === false) {
+  console.log(`Number ${raceNumber}, your race will begin at 11 AM.`);
+} else if (runnerAge < 18) {
+  console.log(`Number ${raceNumber}, your race will begin at 12:30 PM.`);
+} else {
+  console.log(
+    `For those that are 18, please head over to the registration desk immediately.`
+  );
+}

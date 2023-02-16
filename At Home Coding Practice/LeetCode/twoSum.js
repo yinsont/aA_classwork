e84e0de7dc13b13bf881c0ca6654f8f67a77602f
Yinson Tso <yinson.tso@gmail.com>
2023-02-16 16:38:05 -0500

    twoSum/beginning leet code

---
+++ At Home Coding Practice/LeetCode/twoSum.js
@@ -0,0 +1,12 @@
+function twoSum(nums,target){
+    for (let i = 0; i < nums.length; i++){
+        let num1 = nums[i]
+        for (let j = i+1; j < nums.length; j++){
+            let num2 = nums[j]
+            if (num1 + num2 === target){
+                return [num1,num2]
+            }
+        }
+    }
+}
+console.log(twoSum([2,7,11,15],9))

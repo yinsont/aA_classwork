72590897236c7761b689a49c0117560a268da3fb
Yinson Tso <yinson.tso@gmail.com>
2023-02-28 21:02:31 -0500

    reverseInt

---
+++ At Home Coding Practice/LeetCode/reverseInteger.js
@@ -0,0 +1,8 @@
+function reverseInt(x) {
+    let str = x.toString().reverse()
+    return str
+}
+
+console.log(reverseInt(123)) //321
+console.log(reverseInt(-123)) //-321
+console.log(reverseInt(120)) //21
